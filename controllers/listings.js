8bfeb85d689e69aec917ec9279f26bd9832b2842
Mansi Swaraj <mansiswaraj26@gmail.com>
2024-06-21 13:00:37 +0530

    updated

--- controllers/listings.js
+++ controllers/listings.js
@@ -16,14 +16,16 @@ const listingSchemaInput = z.object({
 });
 
 // Define Joi schema for listing
-const listingSchemaJoi = Joi.object({
-  title: Joi.string().required(),
-  description: Joi.string().required(),
-  price: Joi.number().required().min(0),
-  location: Joi.string().required(),
-  country: Joi.string().required(),
-  image: Joi.string().allow("", null)
-}).required();
+const listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    image:Joi.string().allow("",null),
+  }).required()
+});
 
 // Middleware to validate listing data using zod
 const validateListing = (req, res, next) => {
