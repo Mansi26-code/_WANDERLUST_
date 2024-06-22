29c6901e95de2ff7d4d3a11604967c2fa83f233f
Mansi Swaraj <mansiswaraj26@gmail.com>
2024-06-22 09:15:31 +0530

    map function not working

--- routes/listing.js
+++ routes/listing.js
@@ -1,33 +1,26 @@
-// routes/listing.js
 const express = require('express');
 const router = express.Router();
-const wrapAsync = require('../utils/wrapAsync.js');
-const Listing = require('../models/listing.js');
-const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
-const listingController = require('../controllers/listings.js');
-const { listingSchema } = require('../schema.js'); // Ensure the path is correct
-
+const wrapAsync = require('../utils/wrapAsync');
+const { isLoggedIn, isOwner, validateListing } = require('../middleware');
+const listingController = require('../controllers/listings');
 const multer = require('multer');
-const { storage } = require('../cloudConfig.js');
-const uploadListRouter = require('../controllers/uploadListing.js');
-const upload = multer({ storage: storage });
+const { storage } = require('../cloudConfig');
+const upload = multer({ storage });
 
 router.route('/')
   .get(wrapAsync(listingController.index))
-
   .post(isLoggedIn, upload.single('image'), validateListing, wrapAsync(listingController.createListing));
-  //.post(isLoggedIn, upload.single('image'), validateListing, wrapAsync(listingController.createListing));
-
 
 router.get('/new', isLoggedIn, listingController.renderNewForm);
 
 router.route('/:id')
   .get(wrapAsync(listingController.showListing))
-  .put(isLoggedIn,upload.single('listing[image]'), isOwner, validateListing, wrapAsync(listingController.updateListing))
+  .put(isLoggedIn, upload.single('image'), isOwner, validateListing, wrapAsync(listingController.updateListing))
   .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 router.get('/:id/edit', isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 module.exports = router;
+
 
 //
